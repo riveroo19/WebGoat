e46896cb86669473feda5c32fb811a26bbc449c8
imarquezz <115017397+imarquezz@users.noreply.github.com>
2024-04-07 21:34:10 +0200

    Update WebSecurityConfig.java

--- src/main/java/org/owasp/webgoat/webwolf/WebSecurityConfig.java
+++ src/main/java/org/owasp/webgoat/webwolf/WebSecurityConfig.java
@@ -20,7 +20,6 @@
  * Source for this application is maintained at https://github.com/WebGoat/WebGoat, a repository for free software projects.
  */
 package org.owasp.webgoat.webwolf;
-
 package org.springframework.security.crypto.bcrypt;
 
 import lombok.AllArgsConstructor;
